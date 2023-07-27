10fde18a147fa515c4ec492f3f9618c5e659bcb3
hyeonjin <kimguswls23@gmail.com>
2023-07-27 13:57:49 +0900

    Link Main and Signup

--- app/src/main/java/kr/ac/tukorea/plannerapp/MainActivity.kt
+++ app/src/main/java/kr/ac/tukorea/plannerapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package kr.ac.tukorea.plannerapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.tukorea.plannerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpTxt.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
