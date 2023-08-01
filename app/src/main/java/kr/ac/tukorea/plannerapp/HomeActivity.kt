c27a3fe58060ce3e393660477c050ec768671a38
groom2hub <groomwnsgur35@gmail.com>
2023-08-01 13:28:09 +0900

    clear git cache

--- app/src/main/java/kr/ac/tukorea/plannerapp/HomeActivity.kt
+++ app/src/main/java/kr/ac/tukorea/plannerapp/HomeActivity.kt
@@ -7,10 +7,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import kr.ac.tukorea.plannerapp.databinding.ActivityHomeBinding
+import android.content.Intent
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 @RequiresApi(Build.VERSION_CODES.O)
 open class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private val backKeyHandler: BackKeyHandler = BackKeyHandler(this) //BackKeyHandler 클래스 인스턴스 생성
+    private val db = Firebase.firestore
     private lateinit var planViewModel: PlanViewModel
     private lateinit var planlistadapter: PlanListAdapter
 
@@ -31,6 +37,32 @@ open class HomeActivity : AppCompatActivity() {
         binding.rvPlan.setHasFixedSize(true)
         binding.rvPlan.layoutManager = planLayoutManager
 
+        if (Firebase.auth.currentUser == null) {
+            var loginIntent = Intent(this, MainActivity::class.java)
+            startActivity(loginIntent)
+        }
+
+        binding.logoutButton.setOnClickListener {
+            Firebase.auth.signOut()
+            var loginIntent = Intent(this, MainActivity::class.java)
+            startActivity(loginIntent)
+        }
+
+        db.collection("users")
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (i in task.result) {
+                        if (i.id == Firebase.auth.currentUser!!.uid) {
+                            val userName = i.data["name"]
+                            binding.userNameText.text = userName.toString()
+                        }
+                    }
+                }
+            }
+    }
+    override fun onBackPressed() { //뒤로 가기 버튼을 두 번 눌러야 앱이 종료
+        backKeyHandler.onBackPressed()
     }
 }
 
